5035363c0cde7057b404e8086e924897af37ffe6
Rocha504 <josuerocha504@gmail.com>
2023-05-24 22:52:50 -0400

    avanzando

--- reactjsproyect/src/App.js
+++ reactjsproyect/src/App.js
@@ -8,12 +8,26 @@ function App() {
     JSON.parse(localStorage.getItem("estado")) || []
   );
 
-  const [newTodo, setNewTodo] = useState("");
+
+  const [otraTarea, setOtraTarea] = useState("");
 
   useEffect(() => {
     localStorage.setItem("estado", JSON.stringify(estado));
   }, [estado]);
+
+  function addTodo (prop){
+    prop.preventDefault();
+    Tareas([...estado, { text: otraTarea, completed: false }]);
+    setOtraTarea("");
+  };
+
+  function hecha (a) {
+    const newTodos = [...estado];
+    newTodos[a].completed = !newTodos[a].completed;
+    setTareas(newTodos);
+  };
   
+
   
   return (
       
