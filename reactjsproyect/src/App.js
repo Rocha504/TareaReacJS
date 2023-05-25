9dbbb97a472e6ce8d121e5e796cd346694158191
Rocha504 <josuerocha504@gmail.com>
2023-05-24 22:35:26 -0400

    almenos tenia algunas cosas guardadas en notepad

--- reactjsproyect/src/App.js
+++ reactjsproyect/src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+   
+  const [estado, Tareas] = useState(
+    JSON.parse(localStorage.getItem("estado")) || []
+  );
+
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("estado", JSON.stringify(estado));
+  }, [estado]);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
   );
 }
 
